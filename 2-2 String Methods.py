e0f54a654f0c3183a55b9cb03003b49c6e2c45bd
Solomon <sony187@howcollege.ac.uk>
2021-05-12 10:05:57 +0100

    Create 2-2 String Methods.py

---
+++ 2-2 String Methods.py
@@ -0,0 +1,10 @@
+# A program to test string methods
+
+phrase = "Are there any women here?"
+
+print("Original phrase:", phrase)
+print("Converted to Uppercase:", phrase.upper())
+print("Converted to lowercase:", phrase.lower())
+print("With emphasis:", phrase.replace("women", "WOMEN"))
+print("There starts at index:", phrase.index("there"))
+ 
